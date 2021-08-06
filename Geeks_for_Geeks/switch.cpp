d8e3f44e8284259d545ef024f51d87e5a85a7d8f
Abhishek.R <71394391+abhishek1BM19CS400@users.noreply.github.com>
2021-08-06 11:57:23 +0530

    Create switch.cpp

---
+++ Geeks_for_Geeks/switch.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+void utility(int number){
+
+    //write your code here
+    switch(number)
+    {
+        case 1:
+        cout<<"One";
+        break;
+        default:
+        cout<<"Not One";
+        break;
+    }
+}
+
+// { Driver Code Starts.
+    
+int main() {
+	
+	int t; cin>>t;
+	while(t--){
+	    int number; cin>>number;
+        utility(number);
+	}
+	
+}  // } Driver Code Ends
